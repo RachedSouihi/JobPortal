fe93261169026eb8976f29e84703c37f85c39788
RachedSouihi <141030088+RachedSouihi@users.noreply.github.com>
2024-04-11 15:56:11 +0100

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -14,12 +14,6 @@ const upload = multer({ storage });
 
 
 
-//const socketIO = require('socket.io');
-
-
-//const socketToUserIdMap = new Map();
-
-
 
 const app = express();
 app.use(cors())
@@ -37,7 +31,7 @@ socketIOServer(server)
 const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
-  password: '242619',
+  password: '',
   database: 'JobPortal',
   waitForConnections: true,
   queueLimit: 0
@@ -54,69 +48,7 @@ pool.getConnection((err, connection) => {
   }
 });
 
-
-/*io.on('connection', (socket) => {
-  console.log(socketToUserIdMap)
-
-  console.log('A user connected');
-
-  socket.on('join', userId => {
-    socket.join(userId)
-    socketToUserIdMap.set(socket.id, userId)
-    
-    
-    console.log("user with ID", userId, " join the socketIO server")
-  })
-
-
-  socket.on('offerDeleted', offerId => {
-    console.log('offerId: ', offerId)
-    io.emit('offerDeleted', offerId)
-  })
-  socket.on('IApplied', (seekerJobData, hiringMgrId) =>{
-    io.to(seekerJobData.userId).emit("IApplied", seekerJobData)
-    io.to(hiringMgrId).emit('someoneApply', seekerJobData)
-
-  })
-
-  socket.on('candidacyEdited', candidacy => {
-    console.log("CA:", candidacy.hiring_mgr_id)
-    
-    io.to(Number(candidacy.hiring_mgr_id)).emit('candidacyEdited', candidacy)
-  })
-
-  socket.on('candidacyDeleted', candidacyPK => {
-    io.to(candidacyPK.userId).emit("candidacyDeletedSeekerJob", candidacyPK.offerId)
-
-    io.to(candidacyPK.hiring_mgr_id).emit("candidacyDeletedHiringManager", candidacyPK)
-
-    
-  })
-
-  socket.on("user-disconnect", userId => {
-    socket.leave(userId)
-    socketToUserIdMap.delete(socket.id)
-    console.log(`User ${userId} disconnected`);
-
-
-  })
-
  
-  socket.on('disconnect', () => {
-    const userId = socketToUserIdMap.get(socket.id)
-    if(userId !== undefined && userId !== null){
-      io.to(userId).emit('UserDisconnected', {})
-      //socketToUserIdMap.delete(socket.id)
-      
-      console.log(`User ${userId} disconnected`);
-
-
-    }
-      console.log('User disconnected');
-      
-  });
-});
-*/
 app.post('/sign-up', (req, res) => {
 
   const data = req.body.userData;
